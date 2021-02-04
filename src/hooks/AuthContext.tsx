329d2bb64873220fc09b2c45a14303e101e1052b
Luiz <luizemarin@gmail.com>
2021-02-03 21:43:56 -0300

    Configure pages and routes

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -25,6 +25,7 @@ interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
@@ -32,6 +33,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 // eslint-disable-next-line react/prop-types
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -43,6 +45,7 @@ export const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+      setLoading(false);
     }
     loadStoragedData();
   }, []);
@@ -69,7 +72,7 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
